7d00818b41d31c3499d5392d7a04e6b4b99c5fc8
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-08-27 13:55:28 +0300

    Noting undestend

---
+++ src/main/java/ru/netology/domain/FilmAtPlaybill.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class FilmAtPlaybill {
+    private int id;
+    private int productId;
+    private String productName;
+
+
+}
--- src/main/java/ru/netology/manager/CartManager.java
+++ src/main/java/ru/netology/manager/CartManager.java
@@ -1,35 +1,87 @@
 package ru.netology.manager;
-import ru.netology.domain.PurchaseItem;
+import ru.netology.domain.FilmAtPlaybill;
 
 public class CartManager {
-    private PurchaseItem[] items = new PurchaseItem[0];
+    private FilmAtPlaybill[] films = new FilmAtPlaybill[0];
 
-    public void add(PurchaseItem item) {
+    public void add(FilmAtPlaybill film) {
         // создаём новый массив размером на единицу больше
-        int length = items.length + 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
+        int length = films.length + 1;
+        FilmAtPlaybill[] tmp = new FilmAtPlaybill[length];
         // itar + tab
         // копируем поэлементно
         // for (int i = 0; i < items.length; i++) {
         //   tmp[i] = items[i];
         // }
-        System.arraycopy(items, 0, tmp, 0, items.length);
+        System.arraycopy(films, 0, tmp, 0, films.length);
         // кладём последним наш элемент
         int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = item;
-        items = tmp;
+        tmp[lastIndex] = film;
+        films = tmp;
     }
 
-    public PurchaseItem[] getAll() {
-        PurchaseItem[] result = new PurchaseItem[items.length];
+
+
+
+
+
+
+
+    public FilmAtPlaybill[] getAll() {
+        FilmAtPlaybill[] result = new FilmAtPlaybill[films.length];
+
+        ...
+  int resultLength;
+        if ??? {
+            resultLength = ???
+        } else {
+            resultLength = ???
+        }
+  ??? result = new ???[resultLength];
+        for (???) {
+            // заполняем result из массива что лежит в поле
+            // в обратном порядке
+        }
+...
+
+
+
+
+
+
+
         // перебираем массив в прямом порядке
         // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
+            int index = films.length - i - 1;
+            result[i] = films[index];
         }
         return result;
+
+
+        //  2 конструктора: пустой и с resultLength. Внутри метода менеджера (как мне ответили на вопрос в чате)
+//  Только почему resultLength красный??
+
+    public CartManager(int resultLength) {
+            this.resultLength = resultLength;
+
+        }
+
+    public CartManager() {
+        }
+
+
+
     }
+
+
+
+
+
+
+
+
+
 
     // наивная реализация
     public void removeById(int id) {
