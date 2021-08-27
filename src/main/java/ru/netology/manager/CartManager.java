511dc03c3e0a258fdbcccd16f50e4ad1bb94bda7
Mariia Sheynova <yellow_kitchen@mail.ru>
2021-08-27 19:50:55 +0300

    Stil nothing undestend

--- src/main/java/ru/netology/manager/CartManager.java
+++ src/main/java/ru/netology/manager/CartManager.java
@@ -22,34 +22,8 @@ public class CartManager {
 
 
 
-
-
-
-
-
     public FilmAtPlaybill[] getAll() {
         FilmAtPlaybill[] result = new FilmAtPlaybill[films.length];
-
-        ...
-  int resultLength;
-        if ??? {
-            resultLength = ???
-        } else {
-            resultLength = ???
-        }
-  ??? result = new ???[resultLength];
-        for (???) {
-            // заполняем result из массива что лежит в поле
-            // в обратном порядке
-        }
-...
-
-
-
-
-
-
-
         // перебираем массив в прямом порядке
         // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
@@ -62,13 +36,13 @@ public class CartManager {
         //  2 конструктора: пустой и с resultLength. Внутри метода менеджера (как мне ответили на вопрос в чате)
 //  Только почему resultLength красный??
 
-    public CartManager(int resultLength) {
-            this.resultLength = resultLength;
-
-        }
-
-    public CartManager() {
-        }
+//    public CartManager(int resultLength) {
+//            this.resultLength = resultLength;
+//
+//        }
+//
+//    public CartManager() {
+//        }
 
 
 
@@ -83,18 +57,4 @@ public class CartManager {
 
 
 
-    // наивная реализация
-    public void removeById(int id) {
-        int length = items.length - 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
-        int index = 0;
-        for (PurchaseItem item : items) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
-            }
-        }
-        // меняем наши элементы
-        items = tmp;
-    }
 }
